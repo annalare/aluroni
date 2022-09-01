98626fd440423caf1c74276b49b05fd4dd9a3556
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-08-31 23:25:05 -0300

    ordenador

--- src/pages/Cardpio/Ordenador/index.tsx
+++ src/pages/Cardpio/Ordenador/index.tsx
@@ -1,10 +1,22 @@
 import styles from "./ordenador.module.scss";
 import opcoes from "./opcoes.json";
+import { useState } from "react";
+import classNames from "classnames";
 export default function Ordenador() {
+  const [aberto, setAberto] = useState(false);
   return (
-    <button className={styles.ordenador}>
+    <button
+      className={styles.ordenador}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)}
+    >
       <span>Ordenar Por</span>
-      <div className={styles.ordenador__options}>
+      <div
+        className={classNames({
+          [styles.ordenador__options]: true,
+          [styles["ordenador__options--ativo"]]: aberto,
+        })}
+      >
         {opcoes.map((opcao) => (
           <div className={styles.ordenador__option} key={opcao.value}>
             {opcao.nome}
