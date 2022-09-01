6187d4e9644be15a69d00a7ad1271c0e32b39dab
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-09-01 00:35:55 -0300

    ordenador

--- src/pages/Cardpio/Ordenador/index.tsx
+++ src/pages/Cardpio/Ordenador/index.tsx
@@ -1,16 +1,33 @@
 import styles from "./ordenador.module.scss";
 import opcoes from "./opcoes.json";
-import { useState } from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
-export default function Ordenador() {
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+
+interface Props {
+  ordenador: string;
+  setOrdenador: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Ordenador({ ordenador, setOrdenador }: Props) {
   const [aberto, setAberto] = useState(false);
+  const nomeOrdenador =
+    ordenador && opcoes.find((opcao) => opcao.value === ordenador)?.nome;
   return (
     <button
-      className={styles.ordenador}
+      className={classNames({
+        [styles.ordenador]: true,
+        [styles["ordenador--ativo"]]: ordenador !== "",
+      })}
       onClick={() => setAberto(!aberto)}
       onBlur={() => setAberto(false)}
     >
-      <span>Ordenar Por</span>
+      <span>{nomeOrdenador || "Ordenar Por"}</span>
+      {aberto ? (
+        <MdKeyboardArrowUp size={20} />
+      ) : (
+        <MdKeyboardArrowDown size={20} />
+      )}
       <div
         className={classNames({
           [styles.ordenador__options]: true,
@@ -18,7 +35,11 @@ export default function Ordenador() {
         })}
       >
         {opcoes.map((opcao) => (
-          <div className={styles.ordenador__option} key={opcao.value}>
+          <div
+            className={styles.ordenador__option}
+            key={opcao.value}
+            onClick={() => setOrdenador(opcao.value)}
+          >
             {opcao.nome}
           </div>
         ))}
